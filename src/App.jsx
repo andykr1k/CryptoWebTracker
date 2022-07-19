52ede2004da927752b2210506c699cd9f3885acf
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-19 14:19:59 -0700

    Fixing Layout and Adding Crypto API

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
-import * as React from 'react'
+import * as React from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
-import { Loader, Header, Footer } from './components'
+import { Loader, Header, Footer, CryptoWatchlist } from './components';
+import { BrowserRouter, Router, Routes } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { motion } from "framer-motion";
 import './App.css'
 
 function App() {
   
   return (
-    <div className='overscroll-none'>
-      <div className='grid place-items-center h-screen'>
+    <BrowserRouter>
+      <div className='overscroll-none'>
+        <div className='grid place-items-center h-screen'>
 
-        <header className='fixed top-0 left-0 w-screen h-12'>
-          <Header />
-        </header>
+          <header className='fixed top-0 left-0 w-screen h-12'>
+            <Header />
+          </header>
 
-        <Loader />
+          <Loader />
 
-        <footer className='fixed bottom-0 left-0 w-screen h-12'>
-          <Footer />
-        </footer>
+          <footer className='fixed bottom-0 left-0 w-screen h-12'>
+            <Footer />
+          </footer>
+
+        </div>
 
       </div>
-
-    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/CryptoWatchlist.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import '../App.css'
+
+
+function CryptoWatchlist() {
+  return (
+    <div>
+        <h1 className=''>Watchlist</h1>
+    </div>
+  )
+}
+
+export default CryptoWatchlist;
