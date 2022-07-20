ab3855585e3283f77fbb6af60042c0dc5d217a8c
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-19 20:15:26 -0700

    Setting up DB

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import { Player } from '@lottiefiles/react-lottie-player';
 import { Loader, Header, Footer, CryptoWatchlist } from './components';
 import { BrowserRouter, Router, Routes } from 'react-router-dom';
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { motion } from "framer-motion";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 import './App.css'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfUXVtjtV5qT_84UrKomJeTMgMteC4wZE",
+  authDomain: "cryptowebtracker.firebaseapp.com",
+  databaseURL: "https://cryptowebtracker-default-rtdb.firebaseio.com",
+  projectId: "cryptowebtracker",
+  storageBucket: "cryptowebtracker.appspot.com",
+  messagingSenderId: "759397487409",
+  appId: "1:759397487409:web:3ba44829222476e2f05610",
+  measurementId: "G-G7J4429VNJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
 
